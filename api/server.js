ab312656aa28726dc5dc7beb5c5604cf2949e254
joeygaitan <joeygaitan707@gmail.com>
2023-04-22 00:57:40 -0700

    added in some server.js intial setup

---
+++ api/server.js
@@ -0,0 +1,30 @@
+// packages
+let express = require("express");
+let cors = require('cors');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+
+// setup
+let router = express.Router();
+let app = express();
+
+// production check
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+// cors and testing
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// port setup
+let port = process.env.PORT || 5000;
+
+// app deployment
+app.listen(port, function () {
+    console.log("Server started on port " + port + " :)");
+});
+
+// export application.
+module.exports = app;
