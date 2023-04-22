7d1dbc9fb8f8e3631032e8202eb6ddf311bf219c
nima64 <kennethe407@gmail.com>
2023-04-22 08:56:51 -0700

    basic homepage layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import LandingPage from './LandingPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={< LandingPage />}/>
+          <Route path="/medicalHistory" element={< LandingPage />}/>
+
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
